e31b3f03659a3eb4b731c7a07bb9df43cdd18cf8
Lyndon Michael Bibera <lyndon.michael.bibera@asia.ing.com>
2019-10-26 22:14:00 +0800

    enabled cors

--- src/main/java/io/headhuntr/authserver/auth/SecurityConfig.java
+++ src/main/java/io/headhuntr/authserver/auth/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().permitAll();
+        http.cors();
     }
 
     @Override
---
+++ src/main/java/io/headhuntr/authserver/shared/WebConfig.java
@@ -0,0 +1,26 @@
+package io.headhuntr.authserver.shared;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class WebConfig {
+
+    //TODO: dont hardcode, use config please
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                log.info("Configuring CORS");
+
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
